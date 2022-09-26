5b499b4a071dbb02dbba4346c9bb46a67f487b76
Gustavo <gustgouvea@hotmail.com>
2022-09-26 11:19:28 -0300

    finalizando estudo

--- estudoreducer/src/counterUseReducer.js
+++ estudoreducer/src/counterUseReducer.js
@@ -1,13 +1,30 @@
-import { useReducer } from "react";
+import { useReducer, useState } from "react";
 
 const initialState = { count: 0 };
 
+const actions = {
+    decrement: 'decrement',
+    increment: 'increment',
+    double: 'multiply',
+    half: 'half',
+    reset: 'reset',
+    custom: 'custom',
+};
+
 function reducer(state, action) {
     switch (action.type) {
-        case 'decrement':
+        case actions.decrement:
             return { count: state.count - 1 };
-        case 'increment':
+        case actions.increment:
             return { count: state.count + 1 };
+        case actions.double:
+            return { count: state.count * 2 };
+        case actions.half:
+            return { count: state.count / 2 };
+        case actions.reset:
+            return { count: initialState.count }
+        case actions.custom:
+            return { count: action.payload }
         default:
             return state;
     }
@@ -15,13 +32,36 @@ function reducer(state, action) {
 
 export function CounterUseReducer() {
     const [state, dispatch] = useReducer(reducer, initialState);
+    const [customCount, setCustomCount] = useState(0);
+
+    function handleSubmit(e) {
+        e.preventDefault();
+        dispatch({ type: actions.custom, payload: customCount })
+    }
 
     return (
         <div>
             <h3>Counter com useReducer</h3>
-            <button onClick={() => dispatch({ type: 'decrement' })}>-</button>
-            <span>{state.count}</span>
-            <button onClick={() => dispatch({ type: 'increment' })}>+</button>
+            <span>Count: {state.count}</span>
+            <br />
+            <button onClick={() => dispatch({ type: actions.decrement })}>-</button>            
+            <button onClick={() => dispatch({ type: actions.increment })}>+</button>
+            <button onClick={() => dispatch({ type: actions.double })}>X2</button>
+            <button onClick={() => dispatch({ type: actions.half })}>/2</button>
+            <button onClick={() => dispatch({ type: actions.reset })}>Reset</button>
+            <br />
+            <br />
+
+            <form onSubmit={handleSubmit}>
+                <label htmlFor="custom_value">Valor personalizado: </label>
+                <input 
+                    id="custom_value" 
+                    type="number" 
+                    value={customCount} 
+                    onChange={(e) => setCustomCount(e.target.value)} 
+                />
+                <button type="submit">Inserir</button>
+            </form>
         </div>
     );
 }
